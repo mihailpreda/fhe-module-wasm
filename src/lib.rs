15333468cafd905df102f7c0074e5ffd5b2ad1d4
Preda Mihail Irinel <mihaipreda1997@gmail.com>
2022-05-02 20:23:59 +0300

    fixed CKKS, cleanup to be done

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(module = "/js/bindings.js")]
 extern "C" {
+    /* SETUP */
+    /********************************************************************************************************* */
     async fn js_to_rust_initialize() -> JsValue;
     fn js_to_rust_set_encryption_scheme(scheme: String) -> JsValue;
     fn js_to_rust_setup_context(
@@ -24,39 +26,41 @@ extern "C" {
         security_level: String,
         processing_speed: String,
     ) -> Vec<JsValue>;
+    /********************************************************************************************************* */
+    /* BASIC */
+    /********************************************************************************************************* */
     fn js_to_rust_generate_keys() -> Vec<JsValue>;
-    // fn js_to_rust_encrypt(array: Vec<i32>, public_key: JsValue) -> JsValue;
-    // fn js_to_rust_decrypt(array: Vec<i32>, secret_key: JsValue) -> JsValue;
-    // fn js_to_rust_add_ciphers(cipher_text1: Vec<i32>, cipher_text2: Vec<i32>) -> JsValue;
-    // fn js_to_rust_sub_ciphers(cipher_text1: Vec<i32>, cipher_text2: Vec<i32>) -> JsValue;
-    // fn js_to_rust_multiply_ciphers(cipher_text1: Vec<i32>, cipher_text2: Vec<i32>) -> JsValue;
-    // fn js_to_rust_square_cipher(cipher_text1: Vec<i32>) -> JsValue;
-    // fn js_to_rust_exponentiate_cipher(cipher_text1: Vec<i32>, power: i32) -> JsValue;
-    // fn js_to_rust_negate_cipher(cipher_text1: Vec<i32>) -> JsValue;
-    // fn js_to_rust_add_plain(cipher_text: Vec<i32>, plain_text: Vec<i32>) -> JsValue;
-    // fn js_to_rust_sub_plain(cipher_text: Vec<i32>, plain_text: Vec<i32>) -> JsValue;
-    // fn js_to_rust_multiply_plain(cipher_text: Vec<i32>, plain_text: Vec<i32>) -> JsValue;
-    // fn js_to_rust_sum_elements(cipher_text1: Vec<i32>, scheme: String) -> JsValue;
-    fn js_to_rust_deallocate_context();
-    fn js_to_rust_deallocate_parameters();
-    fn js_to_rust_deallocate_seal_library();
-    fn js_to_rust_deallocate_module();
-
     fn js_to_rust_encrypt(array: Vec<i32>, public_key: JsValue) -> JsValue;
     fn js_to_rust_decrypt(array: String, secret_key: JsValue) -> JsValue;
+    /********************************************************************************************************* */
+    /* CIPHER */
+    /********************************************************************************************************* */
     fn js_to_rust_add_ciphers(cipher_text1: String, cipher_text2: String) -> JsValue;
     fn js_to_rust_sub_ciphers(cipher_text1: String, cipher_text2: String) -> JsValue;
     fn js_to_rust_multiply_ciphers(cipher_text1: String, cipher_text2: String) -> JsValue;
     fn js_to_rust_square_cipher(cipher_text1: String) -> JsValue;
     fn js_to_rust_exponentiate_cipher(cipher_text1: String, power: i32) -> JsValue;
     fn js_to_rust_negate_cipher(cipher_text1: String) -> JsValue;
+    /********************************************************************************************************* */
+    /* PLAIN */
+    /********************************************************************************************************* */
     fn js_to_rust_add_plain(cipher_text: String, plain_text: Vec<i32>) -> JsValue;
     fn js_to_rust_sub_plain(cipher_text: String, plain_text: Vec<i32>) -> JsValue;
     fn js_to_rust_multiply_plain(cipher_text: String, plain_text: Vec<i32>) -> JsValue;
+    /********************************************************************************************************* */
+    /* MEMORY MANAGEMENT */
+    /********************************************************************************************************* */
+    fn js_to_rust_deallocate_context();
+    fn js_to_rust_deallocate_parameters();
+    fn js_to_rust_deallocate_seal_library();
+    fn js_to_rust_deallocate_module();
+    /********************************************************************************************************* */
+    /* EXPERIMENTAL */
     fn js_to_rust_sum_elements(cipher_text1: String, scheme: String) -> JsValue;
-
+    /********************************************************************************************************* */
 }
-
+/* SETUP */
+/********************************************************************************************************* */
 #[wasm_bindgen]
 pub async fn rust_initialize() -> JsValue {
     let result = js_to_rust_initialize().await;
@@ -79,7 +83,9 @@ pub fn rust_setup_context(
 pub fn rust_fast_setup(scheme: String, security_level: String, processing_speed: String) {
     js_to_rust_fast_setup(scheme, security_level, processing_speed);
 }
-
+/********************************************************************************************************* */
+/* BASIC */
+/********************************************************************************************************* */
 #[wasm_bindgen]
 pub fn rust_generate_keys() -> Vec<JsValue> {
     let result = js_to_rust_generate_keys();
@@ -97,7 +103,9 @@ pub fn rust_decrypt(array: String, secret_key: JsValue) -> JsValue {
     let result = js_to_rust_decrypt(array, secret_key);
     result
 }
-
+/********************************************************************************************************* */
+/* CIPHER */
+/********************************************************************************************************* */
 #[wasm_bindgen]
 pub fn rust_add_ciphers(cipher_text1: String, cipher_text2: String) -> JsValue {
     let result = js_to_rust_add_ciphers(cipher_text1, cipher_text2);
@@ -132,7 +140,9 @@ pub fn rust_negate_cipher(cipher_text1: String) -> JsValue {
     let result = js_to_rust_negate_cipher(cipher_text1);
     result
 }
-
+/********************************************************************************************************* */
+/* PLAIN */
+/********************************************************************************************************* */
 #[wasm_bindgen]
 pub fn rust_add_plain(cipher_text: String, plain_text: Vec<i32>) -> JsValue {
     let result = js_to_rust_add_plain(cipher_text, plain_text);
@@ -150,7 +160,9 @@ pub fn rust_multiply_plain(cipher_text: String, plain_text: Vec<i32>) -> JsValue
     let result = js_to_rust_multiply_plain(cipher_text, plain_text);
     result
 }
-
+/********************************************************************************************************* */
+/* MEMORY MANAGEMENT */
+/********************************************************************************************************* */
 #[wasm_bindgen]
 pub fn rust_deallocate_context() {
     js_to_rust_deallocate_context()
@@ -170,7 +182,9 @@ pub fn rust_deallocate_seal_library() {
 pub fn rust_deallocate_module() {
     js_to_rust_deallocate_module()
 }
-
+/********************************************************************************************************* */
+/* EXPERIMENTAL */
+/********************************************************************************************************* */
 #[wasm_bindgen]
 pub fn rust_sum_elements(cipher_text1: String, scheme: String) -> JsValue {
     let result = js_to_rust_sum_elements(cipher_text1, scheme);
